a0a596d0825524e4662d0cb79c29090b84f06f44
Edy Segura <ednardo@inatel.br>
2016-06-11 11:18:54 -0300

    Add more test cases

--- test/FactorialServiceSpec.js
+++ test/FactorialServiceSpec.js
@@ -2,7 +2,15 @@
 describe('FactorialService.js', function() {
     
     it('should calculate the factorial from a given number', function() {
+        expect(FactorialService.calculate(1)).toBe(1);
+        expect(FactorialService.calculate(2)).toBe(2);
+        expect(FactorialService.calculate(3)).toBe(6);
+        expect(FactorialService.calculate(4)).toBe(24);
         expect(FactorialService.calculate(5)).toBe(120);
+    });
+    
+    it('should return 1 (one) if the given number is 0 (zero)', function() {
+        expect(FactorialService.calculate(0)).toBe(1);
     });
         
 });

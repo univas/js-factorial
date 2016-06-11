70c65ed1b286c3eae12cbd1b5eabca8dc9a86436
Edy Segura <ednardo@inatel.br>
2016-06-11 11:46:07 -0300

    Making tests pass

--- app/js/FactorialService.js
+++ app/js/FactorialService.js
@@ -1,6 +1,13 @@
 var FactorialService = (function () {
+
+    function isNumberValid(number) {
+        return (number !== undefined)
+            && (number > -1)
+            && (typeof(number) !== 'string');
+    }
     
     function calculate(number) {
+        if(!isNumberValid(number)) return null;
         var result = 1;
         if(number === 0 || number === 1) return result;
 

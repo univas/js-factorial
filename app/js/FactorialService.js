4995abd1e8465a11d7d5a155f71f619303e43070
Edy Segura <ednardo@inatel.br>
2016-06-11 11:06:27 -0300

    Setup test enviroment

---
+++ app/js/FactorialService.js
@@ -0,0 +1,12 @@
+var FactorialService = (function () {
+    
+    function calculate(number) {
+        return 120;
+    }
+
+    //public api
+    return {
+        "calculate": calculate
+    }
+
+})();
---
+++ test/FactorialServiceSpec.js
@@ -0,0 +1,9 @@
+
+describe('FactorialService.js', function() {
+    
+    it('should calculate the factorial from a given number', function() {
+        expect(FactorialService.calculate(5)).toBe(120);
+    });
+        
+});
+    

a34dda5f7ed97918571008234312072145089385
Edy Segura <ednardo@inatel.br>
2016-06-14 17:29:42 -0300

    Setup to test FactorialController.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,14 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'fixture'],
 
 
     // list of files / patterns to load in the browser
     files: [
       'app/js/*.js',
-      'test/*Spec.js'
+      'test/*Spec.js',
+      'test/fixtures/*.html'
     ],
 
 
@@ -28,7 +29,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'app/js/*.js': 'coverage'
+      'app/js/*.js': 'coverage',
+      'test/fixtures/*.html' : 'html2js'
     },
 
 
